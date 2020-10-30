39d7f17e2ffe7de758f46b16447a46fecad69ffc
Satan <11800460@student.pxl.be>
2020-10-30 10:54:44 +0100

    added record class as 'dossier', changed some classes into enums instead.

---
+++ BackEnd/eMenka/eMenka.Domain/Classes/Company.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eMenka.Domain.Classes
+{
+    public class Company
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BackEnd/eMenka/eMenka.Domain/Enums/Term.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eMenka.Domain.Enums
+{
+    public enum Term
+    {
+        Long,
+        Short
+    }
+}
