0615181c01fb47853e1e1f677ff08e4032cb3853
Arne Liekens <arne.liekens@student.pxl.be>
2020-10-30 14:03:14 +0100

    added some more relations to vehicle

--- BackEnd/eMenka/eMenka.Domain/Classes/Vehicle.cs
+++ BackEnd/eMenka/eMenka.Domain/Classes/Vehicle.cs
@@ -7,11 +7,14 @@ namespace eMenka.Domain.Classes
     {
         public int Id { get; set; }
         public Brand Brand { get; set; }
+        public int BrandId { get; set; }
         public int ModelId { get; set; }
         public Model Model { get; set; }
+        public int MotorTypeId { get; set; }
         public MotorType MotorType { get; set; }
         public FuelCard FuelCard { get; set; }
         public int FuelCardId { get; set; }
+        public int DoorTypeId { get; set; }
         public DoorType DoorType { get; set; }
         public FuelType FuelType { get; set; }
         public int Volume { get; set; }
@@ -20,6 +23,5 @@ namespace eMenka.Domain.Classes
         public int Power { get; set; }
         public DateTime EndDate { get; set; }
         public bool IsActive { get; set; }
-
     }
 }
