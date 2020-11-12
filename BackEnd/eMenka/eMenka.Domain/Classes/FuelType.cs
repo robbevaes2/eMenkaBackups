a05ade75ff283f46c5a33dbd815d820d510dbe5a
MiloszBoghePXL <11800460@student.pxl.be>
2020-11-12 11:40:34 +0100

    fueltype.cs

---
+++ BackEnd/eMenka/eMenka.Domain/Classes/FuelType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace eMenka.Domain.Classes
+{
+    public class FuelType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
