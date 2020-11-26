df33fa73fae833b68cb5730cea51efa6ff8f9ed5
AlexanderDewaelheynsPXL <alexander.dewaelheyns@student.pxl.be>
2020-11-26 13:33:28 +0100

    Fixed issue with searching in datatable

--- eFenkaFrontend/src/app/components/vehicle-list/vehicle-list.component.ts
+++ eFenkaFrontend/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -12,6 +12,7 @@ import { MdbTableDirective, MdbTablePaginationComponent } from 'angular-bootstra
 import { VehicleService } from '../../services/vehicle-service';
 import { EngineType } from 'src/app/models/engine-type/engine-type';
 import { ApiService } from '../../services/api.service';
+import { Serie } from 'src/app/models/serie/serie';
 
 
 @Component({
@@ -22,7 +23,7 @@ import { ApiService } from '../../services/api.service';
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
   headNames  = ["Merk", "Model", "brandstof", "type motor", "aantal deuren", "volume", "fiscale Pk", "vermogen", "Einddatum"];
-  headElements  = ["brand.name", "model.name", "fuelType.name", "motorType.name", "doorType.name", "volume", "fiscalePk", "power", "endData"];
+  headElements  = ["brand.name", "model.name", "fuelType.name", "motorType.name", "doorType.name", "volume", "fiscalHP", "power", "endDateDelivery"];
 
   @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
   @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
@@ -37,17 +38,21 @@ export class VehicleListComponent implements OnInit {
 
   ngOnInit(): void {
     this.apiService.getAllVehicles().subscribe(data => this.vehicles = data);
+    //this.vehicles = this.getVehicleDummyList();
+
+  }
+
+  ngAfterViewInit() {
 
     this.mdbTable.setDataSource(this.vehicles);
     this.vehicles = this.mdbTable.getDataSource();
     this.previous = this.mdbTable.getDataSource();
-  }
-
-  ngAfterViewInit() {
+    /*
     this.mdbTablePagination.setMaxVisibleItemsNumberTo(this.maxVisibleItems);
 
     this.mdbTablePagination.calculateFirstItemIndex();
     this.mdbTablePagination.calculateLastItemIndex();
+    */
     this.cdRef.detectChanges();
   }
 
@@ -71,11 +76,36 @@ export class VehicleListComponent implements OnInit {
     }
   }
 
-  /*getVehicleDummyList(): Vehicle[] {
->>>>>>> de467508d341516cabb0e19c0569640ffe9dad7f
+  getVehicleDummyList(): Vehicle[] {
       return [
         {
           id: 1,
+          brand: new Brand(1, 'BMW'),
+          model: new Model(1, 'M5'),
+          fuelType: new FuelType(1, 'Benzine'),
+          engineType: new EngineType(1, '1.9 JTD'),
+          doorType: new DoorType(1, '5-deurs'),
+          fuelCard: new FuelCard(1, 'feazfazefazefazef', null, null, null, null, null, true),
+          volume: 2000,
+          fiscalHP: 50,
+          emission: 1,
+          power: 300,
+          licensePlate: '1-abc-123',
+          endDateDelivery: new Date('2020-01-16'),
+          isActive: true,
+          chassis: 'feoipajfpoaezfjipio',
+          registrationDate: new Date('2020-01-16'),
+          engineCapacity: null,
+          enginePower: null,
+          category: null,
+          averageFuel: null,
+          serie: new Serie(1, 'test'),
+          country: new Country(1, 'test', 'set', 'test', true, true),
+          buildYear: 1990,
+          kilometers: 100000
+        },
+        {
+          id: 2,
           brand: new Brand(1, 'BMW'),
           model: new Model(1, 'M4'),
           fuelType: new FuelType(1, 'Benzine'),
@@ -94,54 +124,40 @@ export class VehicleListComponent implements OnInit {
           engineCapacity: null,
           enginePower: null,
           category: null,
-          averageFuel: null
-        },
-        {
-          id: 2,
-          brand: new Brand(2, 'Mercedes'),
-          model: new Model(2, 'AMG GTR'),
-          fuelType: new FuelType(2, 'Diesel'),
-          engineType: new EngineType(2, '1.9 JTD'),
-          doorType: new DoorType(2, '3-deurs'),
-          fuelCard: new FuelCard(1, 'feazfazefazefazef', null, null, null, null, null, true),
-          volume: 2000,
-          fiscalHP: 50,
-          emission: 3,
-          power: 400,
-          licensePlate: '1-abc-124',
-          endDateDelivery: new Date('2020-12-20'),
-          isActive: true,
-          chassis: 'feoipajfpoaezfjipio',
-          registrationDate: new Date('2020-01-16'),
-          engineCapacity: null,
-          enginePower: null,
-          category: null,
-          averageFuel: null
+          averageFuel: null,
+          serie: new Serie(1, 'test'),
+          country: new Country(1, 'test', 'set', 'test', true, true),
+          buildYear: 1990,
+          kilometers: 100000
         },
         {
           id: 3,
-          brand: new Brand(2, 'Audi'),
-          model: new Model(2, 'RS7'),
-          fuelType: new FuelType(2, 'Diesel'),
-          engineType: new EngineType(2, '1.9 JTD'),
-          doorType: new DoorType(2, '5-deurs'),
+          brand: new Brand(1, 'BMW'),
+          model: new Model(1, 'M4'),
+          fuelType: new FuelType(1, 'Benzine'),
+          engineType: new EngineType(1, '1.9 JTD'),
+          doorType: new DoorType(1, '5-deurs'),
           fuelCard: new FuelCard(1, 'feazfazefazefazef', null, null, null, null, null, true),
           volume: 2000,
           fiscalHP: 50,
-          emission: 3,
-          power: 500,
-          licensePlate: '1-abc-124',
-          endDateDelivery: new Date('2020-12-20'),
+          emission: 1,
+          power: 300,
+          licensePlate: '1-abc-123',
+          endDateDelivery: new Date('2020-01-16'),
           isActive: true,
           chassis: 'feoipajfpoaezfjipio',
           registrationDate: new Date('2020-01-16'),
           engineCapacity: null,
           enginePower: null,
           category: null,
-          averageFuel: null
+          averageFuel: null,
+          serie: new Serie(1, 'test'),
+          country: new Country(1, 'test', 'set', 'test', true, true),
+          buildYear: 1990,
+          kilometers: 100000
         }
       ];
-  }*/
+  }
 
   dateToStringConverter(date: string) {
     return new Date(date).toLocaleDateString();
