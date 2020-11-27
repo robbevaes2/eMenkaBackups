f387693365ca3f837d39c3b5c54a2cfc11152fdb
Arne Liekens <arne.liekens@student.pxl.be>
2020-11-27 11:29:01 +0100

    fixed coverage of domain project

---
+++ BackEnd/eMenka/eMenka.Tests/Classes/DriverTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eMenka.Domain.Classes;
+using NUnit.Framework;
+
+namespace eMenka.Tests.Classes
+{
+    [TestFixture]
+    public class DriverTests
+    {
+        private Driver _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new Driver();
+        }
+
+        [Test]
+        public void DriverFuelCardIdPropertyGetsAndSetsFuelCardId()
+        {
+            var fuelCardId = 1;
+
+            _sut.FuelCardId = fuelCardId;
+
+            Assert.That(_sut.FuelCardId, Is.EqualTo(fuelCardId));
+        }
+
+        [Test]
+        public void DriverFuelCardPropertyGetsAndSetsFuelCard()
+        {
+            var fuelCard = new FuelCard();
+
+            _sut.FuelCard = fuelCard;
+
+            Assert.That(_sut.FuelCard, Is.EqualTo(fuelCard));
+        }
+    }
+}
