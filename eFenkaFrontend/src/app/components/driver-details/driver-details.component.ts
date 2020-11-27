b13a5c8001a6ef6a37a59b8d4d79ea748d7cf071
JimmyVaneyck <11502348@student.pxl.be>
2020-11-27 14:15:12 +0100

    Fixed tslint problems

--- eFenkaFrontend/src/app/components/driver-details/driver-details.component.ts
+++ eFenkaFrontend/src/app/components/driver-details/driver-details.component.ts
@@ -21,7 +21,7 @@ export class DriverDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService) { }
 
   ngOnInit(): void {
-    const driverId = this.route.snapshot.params['index'];
+    const driverId = this.route.snapshot.params.index;
     this.apiService.getDriverById(driverId).subscribe(data => {
 
       this.selectedDriver = data;
@@ -46,43 +46,43 @@ export class DriverDetailsComponent implements OnInit {
   }
 
   fillForm(): void {
-    this.form.controls['title'].setValue(this.selectedDriver.person.title);
-    this.form.controls['firstName'].setValue(this.selectedDriver.person.firstname);
-    this.form.controls['lastName'].setValue(this.selectedDriver.person.lastname);
-    this.form.controls['birthDate'].setValue(new Date(this.selectedDriver.person.birthDate).toISOString().substring(0, 10));
-    this.form.controls['gender'].setValue(this.selectedDriver.person.gender);
-    this.form.controls['language'].setValue(this.selectedDriver.person.language);
-    this.form.controls['driverLicenseNumber'].setValue(this.selectedDriver.person.driversLicenseNumber);
-    this.form.controls['driverLicenseType'].setValue(this.selectedDriver.person.driversLicenseType);
-    this.form.controls['startDate'].setValue(new Date(this.selectedDriver.startDate).toISOString().substring(0, 10));
-    this.form.controls['endDate'].setValue(new Date(this.selectedDriver.endDate).toISOString().substring(0, 10));
+    this.form.controls.title.setValue(this.selectedDriver.person.title);
+    this.form.controls.firstName.setValue(this.selectedDriver.person.firstname);
+    this.form.controls.lastName.setValue(this.selectedDriver.person.lastname);
+    this.form.controls.birthDate.setValue(new Date(this.selectedDriver.person.birthDate).toISOString().substring(0, 10));
+    this.form.controls.gender.setValue(this.selectedDriver.person.gender);
+    this.form.controls.language.setValue(this.selectedDriver.person.language);
+    this.form.controls.driverLicenseNumber.setValue(this.selectedDriver.person.driversLicenseNumber);
+    this.form.controls.driverLicenseType.setValue(this.selectedDriver.person.driversLicenseType);
+    this.form.controls.startDate.setValue(new Date(this.selectedDriver.startDate).toISOString().substring(0, 10));
+    this.form.controls.endDate.setValue(new Date(this.selectedDriver.endDate).toISOString().substring(0, 10));
   }
 
   disableForm(): void {
-    this.form.controls['title'].disable();
-    this.form.controls['firstName'].disable();
-    this.form.controls['lastName'].disable();
-    this.form.controls['birthDate'].disable();
-    this.form.controls['gender'].disable();
-    this.form.controls['language'].disable();
-    this.form.controls['driverLicenseNumber'].disable();
-    this.form.controls['driverLicenseType'].disable();
-    this.form.controls['startDate'].disable();
-    this.form.controls['endDate'].disable();
+    this.form.controls.title.disable();
+    this.form.controls.firstName.disable();
+    this.form.controls.lastName.disable();
+    this.form.controls.birthDate.disable();
+    this.form.controls.gender.disable();
+    this.form.controls.language.disable();
+    this.form.controls.driverLicenseNumber.disable();
+    this.form.controls.driverLicenseType.disable();
+    this.form.controls.startDate.disable();
+    this.form.controls.endDate.disable();
     this.isEditable = false;
   }
 
   enableForm(): void {
-    this.form.controls['title'].enable();
-    this.form.controls['firstName'].enable();
-    this.form.controls['lastName'].enable();
-    this.form.controls['birthDate'].enable();
-    this.form.controls['gender'].enable();
-    this.form.controls['language'].enable();
-    this.form.controls['driverLicenseNumber'].enable();
-    this.form.controls['driverLicenseType'].enable();
-    this.form.controls['startDate'].enable();
-    this.form.controls['endDate'].enable();
+    this.form.controls.title.enable();
+    this.form.controls.firstName.enable();
+    this.form.controls.lastName.enable();
+    this.form.controls.birthDate.enable();
+    this.form.controls.gender.enable();
+    this.form.controls.language.enable();
+    this.form.controls.driverLicenseNumber.enable();
+    this.form.controls.driverLicenseType.enable();
+    this.form.controls.startDate.enable();
+    this.form.controls.endDate.enable();
     this.isEditable = true;
   }
 
