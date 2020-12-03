da149672ddfa2d56e2f7132a163ecf669bd34510
AlexanderDewaelheynsPXL <alexander.dewaelheyns@student.pxl.be>
2020-12-03 16:11:03 +0100

    Changed chart colours
    Replaced random chart colours with static colour list

--- eFenkaFrontend/src/app/components/dashboard/dashboard.component.ts
+++ eFenkaFrontend/src/app/components/dashboard/dashboard.component.ts
@@ -28,7 +28,17 @@ export class DashboardComponent implements OnInit {
 
   public chartColors: Array<any> = [
     {
-      backgroundColor: [],
+      backgroundColor: [
+        '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#80B300',
+        '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99',  
+        '#809900', '#E6B3B3', '#6680B3', '#66991A', '#B34D4D',
+        '#FF1A66', '#E6331A', '#33FFCC', '#FF99E6', '#CCFF1A',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',     
+		    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
       borderWidth: 2,
     }
   ];
@@ -97,9 +107,11 @@ export class DashboardComponent implements OnInit {
               label: "Aantal voertuigen per merk"
             }]
     
+            /*
             this.chartLabels.forEach(element => {
               this.chartColors[0].backgroundColor.push(this.getRandomColor());
-            });
+            })*/
+            ;
           }
         )   
         
@@ -117,10 +129,6 @@ export class DashboardComponent implements OnInit {
               data: this.dataArray,
               label: "Aantal voertuigen per categorie"
             }]
-    
-            this.categoryChartLabels.forEach(element => {
-              this.chartColors[0].backgroundColor.push(this.getRandomColor());
-            });
           }
         )
       }
