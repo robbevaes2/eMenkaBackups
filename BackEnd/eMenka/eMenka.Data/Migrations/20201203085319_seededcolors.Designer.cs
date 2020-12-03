27fd8fad58900e3ab3c5450066881fcba2b2fbf8
Arne Liekens <arne.liekens@student.pxl.be>
2020-12-03 09:56:35 +0100

    colors get seeded to vehicle

--- BackEnd/eMenka/eMenka.Data/Migrations/20201203085319_seededcolors.Designer.cs
+++ BackEnd/eMenka/eMenka.Data/Migrations/20201203085319_seededcolors.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eMenka.Data;
 
 namespace eMenka.Data.Migrations
 {
     [DbContext(typeof(EfenkaContext))]
-    partial class EfenkaContextModelSnapshot : ModelSnapshot
+    [Migration("20201203085319_seededcolors")]
+    partial class seededcolors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
