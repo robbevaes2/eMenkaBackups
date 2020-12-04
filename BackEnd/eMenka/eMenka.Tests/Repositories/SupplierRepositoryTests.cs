6ca586393c8828e47fc9eb4969377f42c772b35f
Arne Liekens <arne.liekens@student.pxl.be>
2020-12-04 17:17:55 +0100

    Completed tests for repositories
    -DriverRepository: GetAllAvailableDrivers Tests added
    
    -VehicleRepository: GetAllAvailableVehicles and GetAllAvailableVehiclesByBrandId tests added
    
    -SupplierRepository tests added

---
+++ BackEnd/eMenka/eMenka.Tests/Repositories/SupplierRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eMenka.Data.Repositories;
+using eMenka.Domain.Classes;
+using eMenka.Tests.Utils;
+using NUnit.Framework;
+
+namespace eMenka.Tests.Repositories
+{
+    [TestFixture]
+    public class SupplierRepositoryTests
+    {
+        private SupplierRepository _sut;
+
+        [Test]
+        public void ConstructorSetsCorrectEfenkaContext() //done by checking if getbyid works
+        {
+            EfenkaContextTestFactory.Create();
+
+            _sut = new SupplierRepository(EfenkaContextTestFactory.EfenkaContext);
+
+            var supplier = new Supplier();
+
+            _sut.Add(supplier);
+
+            var supplierFromDatabase = _sut.GetById(supplier.Id);
+
+            Assert.That(supplierFromDatabase, Is.EqualTo(supplier));
+        }
+    }
+}
