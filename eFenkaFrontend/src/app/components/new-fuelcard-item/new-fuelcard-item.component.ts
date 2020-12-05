430e64c194ddeebcaaa509e97aa7fea3999bf332
joren <joren.vanderzande@student.pxl.be>
2020-12-05 14:37:20 +0100

    Fixed two bugs when making and editing a fuelcard
    -Fixed a bug where start date could be after enddate when making a fuelcard
    -Fixed a bug where start date could be after enddate when editing a fuelcard

--- eFenkaFrontend/src/app/components/new-fuelcard-item/new-fuelcard-item.component.ts
+++ eFenkaFrontend/src/app/components/new-fuelcard-item/new-fuelcard-item.component.ts
@@ -7,6 +7,7 @@ import {DatePipe} from '@angular/common';
 import {Driver} from '../../models/driver/driver';
 import {Company} from '../../models/company/company';
 import { pincodeValidator } from 'src/app/services/pincode.validator';
+import { fromToDate } from 'src/app/services/from-to-date.validator';
 
 @Component({
   selector: 'app-new-fuelcard-item',
@@ -48,8 +49,10 @@ export class NewFuelcardItemComponent implements OnInit {
       driver: new FormControl(null, [Validators.required]),
       vehicle: new FormControl(null, [Validators.required]),
       company: new FormControl(null, [Validators.required]),
-      startDate: new FormControl(null, [Validators.required, Validators.min(0)]),
-      endDate: new FormControl(null, [Validators.required, Validators.min(0)]),
+      duration: new FormGroup({
+        startDate: new FormControl(null, [Validators.required, Validators.min(0)]),
+        endDate: new FormControl(null, [Validators.required, Validators.min(0)]),
+      }, {validators: fromToDate}),
       pinCode: new FormControl(null, [Validators.required, pincodeValidator]),
       number: new FormControl(null, [Validators.required])
     });
@@ -72,15 +75,17 @@ export class NewFuelcardItemComponent implements OnInit {
       DriverId: Number(values.driver),
       VehicleId: Number(values.vehicle),
       CompanyId: Number(values.company),
-      StartDate: values.startDate,
-      EndDate: values.endDate,
+      StartDate: values.duration.startDate,
+      EndDate: values.duration.endDate,
       PinCode: values.pinCode,
       Number: values.number
     };
   }
 
   fillForm(): void {
-    this.form.controls.startDate.setValue(this.datepipe.transform(new Date(), 'yyyy-MM-dd'));
-    this.form.controls.endDate.setValue(this.datepipe.transform(new Date(), 'yyyy-MM-dd'));
+    this.form.controls.duration.patchValue({
+      startDate: this.datepipe.transform(new Date(), 'yyyy-MM-dd'),
+      endDate: this.datepipe.transform(new Date(), 'yyyy-MM-dd')
+    });
   }
 }
